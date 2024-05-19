84b45d11258009ea1046536322b86f886d70b822
KB-231Kasilov <vyacheslav.kasilov631842@mail.ru>
2024-05-19 12:40:12 +0300

    swapMaxMinRows

--- libs/algorithms/algorithm.c
+++ libs/algorithms/algorithm.c
@@ -1,3 +1,8 @@
-//
-// Created by User on 23.04.2024.
-//
+#include "algorithm.h"
+
+void swapMaxMinRows(matrix *m) {
+    position max_value = getMaxValuePos(*m);
+    position min_value = getMinValuePos(*m);
+    swapRows(*m, max_value.rowIndex, min_value.rowIndex);
+}
+
--- libs/algorithms/algorithm.h
+++ libs/algorithms/algorithm.h
@@ -1,8 +1,18 @@
-//
-// Created by User on 23.04.2024.
-//
-
 #ifndef UNTITLED_ALGORITHM_H
 #define UNTITLED_ALGORITHM_H
+
+#include "../data_structures/matrix/matrix.h"
+
+// меняет местами строки матрицы m, в которых находятся максимальный и минимальный элементы
+void swapMaxMinRows(matrix *m);
+
+
+
+
+
+
+
+
+
 
 #endif //UNTITLED_ALGORITHM_H
