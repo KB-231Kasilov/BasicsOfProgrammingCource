85ddf6c294ff4c080f26ba4b06e44958fee76901
KB-231Kasilov <vyacheslav.kasilov631842@mail.ru>
2024-05-19 13:05:43 +0300

    sortRowsByMaxElements

--- libs/algorithms/algorithm.h
+++ libs/algorithms/algorithm.h
@@ -6,13 +6,11 @@
 // меняет местами строки матрицы m, в которых находятся максимальный и минимальный элементы
 void swapMaxMinRows(matrix *m);
 
+// возвращает максимальный элемент массива a размера n
+int getMax(int *a, const int n);
 
-
-
-
-
-
-
+// сортирует строки матрицы m по неубыванию наибольших элементов строк
+void sortRowsByMaxElem(matrix *m);
 
 
 #endif //UNTITLED_ALGORITHM_H
